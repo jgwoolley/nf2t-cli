1b53a7025f9757dd885b21c0fe9c8059fcc7cfc3
James W <26191568+jgwoolley@users.noreply.github.com>
2024-12-29 18:00:06 -0500

    Got Maven Central Repository working

--- README.md
+++
@@ -1,2 +0,0 @@
-# nf2t-cli
-A Java CLI for parsing Apache NiFi FlowFiles.
--- build.sh
+++ build.sh
@@ -2,28 +2,37 @@ set -e
 
 mvn clean install
 
-VERSION="0.0.2-SNAPSHOT"
-REPOSITORY_PATH="com/yelloowstone/nf2t/cli/$VERSION/"
+VERSION="0.0.2"
+REPOSITORY_PATH="com/yelloowstone/nf2t/nf2t-cli/$VERSION/"
+POM_TARGET_PATH="./target/nf2t-cli-0.0.2.pom"
 
 mkdir --parents "$REPOSITORY_PATH"
 rm -f "$REPOSITORY_PATH*"
 
-for file in ./target/*.jar; do
-	echo $file
-	gpg --yes --local-user 0xCED254CF741FE1663B9BEC32D12C9545C6D5AA73 -ab $file
-	md5sum $file | cut -d ' ' -f 1 > "$file.md5"
-	sha1sum $file | cut -d ' ' -f 1 > "$file.sha1"
-	sha256sum $file | cut -d ' ' -f 1 > "$file.sha256"
-	sha512sum $file | cut -d ' ' -f 1 > "$file.sha512"
+generate_file() {
+	echo "$1"
+	gpg --yes --local-user 0xCED254CF741FE1663B9BEC32D12C9545C6D5AA73 -ab $1
+	md5sum $1 | cut -d ' ' -f 1 > "$1.md5"
+	sha1sum $1 | cut -d ' ' -f 1 > "$1.sha1"
+	sha256sum $1 | cut -d ' ' -f 1 > "$1.sha256"
+	sha512sum $1 | cut -d ' ' -f 1 > "$1.sha512"
 
-	cp "$file" "./$REPOSITORY_PATH"
-	cp "$file.md5" "./$REPOSITORY_PATH"
-	cp "$file.sha1" "./$REPOSITORY_PATH"
-	cp "$file.sha256" "./$REPOSITORY_PATH"
-	cp "$file.asc" "./$REPOSITORY_PATH"
+	cp "$1" "./${REPOSITORY_PATH}"
+	cp "$1.md5" "./${REPOSITORY_PATH}"
+	cp "$1.sha1" "./${REPOSITORY_PATH}"
+	cp "$1.sha256" "./${REPOSITORY_PATH}"
+	cp "$1.asc" "./${REPOSITORY_PATH}"
+}
+
+for file in ./target/*.jar; do
+	generate_file $file
 done
 
-cp pom.xml "./$REPOSITORY_PATH"
+cp "pom.xml" "$POM_TARGET_PATH"
+
+generate_file "$POM_TARGET_PATH"
+
+rm -f ./maven.zip
 
 zip -r ./maven.zip "$REPOSITORY_PATH"
 rm -rf "$REPOSITORY_PATH"
