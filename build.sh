114a3e71705836fb5850814b40ee7893cdd98623
James Woolley <26191568+jgwoolley@users.noreply.github.com>
2025-03-07 05:07:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ rm *.mvnhelp
 
 mvn install 
 java -jar build-maven-central-zip/target/build-maven-central-zip-${version}.jar docs build-maven-central-zip nf2t-cli
-java -jar build-maven-central-zip/target/build-maven-central-zip-${version}.jar mavenCentral --gpgUser 0xCED254CF741FE1663B9BEC32D12C9545C6D5AA73 build-maven-central-zip nf2t-cli
+java -jar build-maven-central-zip/target/build-maven-central-zip-${version}.jar mavenCentral build-maven-central-zip nf2t-cli
