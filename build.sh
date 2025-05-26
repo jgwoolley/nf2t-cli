1641c64cbaed41a60067b66695fd8ad8aac52b42
James Woolley <26191568+jgwoolley@users.noreply.github.com>
2025-05-26 15:36:16 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,11 +20,11 @@ else
 fi
 
 if [ "$1" == "server" ]; then
-    java -jar setup-project/target/setup-project-${version}.jar mavenCentral setup-project nf2t-cli
+    java -jar nf2t-cli/target/nf2t-cli-${version}.jar mavenCentral nf2t-cli
 else 
-    java -jar setup-project/target/setup-project-${version}.jar mavenCentral --gpgUser 0xCED254CF741FE1663B9BEC32D12C9545C6D5AA73 setup-project nf2t-cli
+    java -jar nf2t-cli/target/nf2t-cli-${version}.jar mavenCentral --gpgUser 0xCED254CF741FE1663B9BEC32D12C9545C6D5AA73 nf2t-cli
 fi 
 
 rm -rf ./dist
 
-java -jar setup-project/target/setup-project-${version}.jar docs setup-project nf2t-cli
+java -jar nf2t-cli/target/nf2t-cli-${version}.jar docs nf2t-cli
